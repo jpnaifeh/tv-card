5dc197d768a83dd9a403a8f06b16718c4bf3b15a
J.P. Naifeh <jp@naifeh.tech>
2025-05-11 20:02:46 -0500

    change back

--- tv-card.js
+++ tv-card.js
@@ -444,7 +444,7 @@ class TVCardServices extends LitElement {
                 toucharea {
                     border-radius: 30px;
                     flex-grow: 1;
-                    height: stretch;
+                    height: -webkit-fill-available;
                     background: #6d767e;
                     touch-action: none;
                     text-align: center;
